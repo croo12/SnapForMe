82693f500793ae9a1b3e9f6cebdeed3b7976e965
croo12 <its19447@gmail.com>
2022-06-09 16:26:42 +0900

    base

---
+++ src/main/java/com/snap/forMe/vo/Field.java
@@ -0,0 +1,6 @@
+package com.snap.forMe.vo;
+
+public class Field {
+    public String field_name;
+    public String field_text;
+}
---
+++ src/main/java/com/snap/forMe/vo/SnapCard.java
@@ -0,0 +1,12 @@
+package com.snap.forMe.vo;
+
+public class SnapCard {
+
+    public String card_name;
+    public int card_energy;
+    public int card_power;
+    public String card_image;
+    public String card_initial_text;
+    public String card_text;
+
+}
